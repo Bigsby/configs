47d0a54e1ea23dd1b7860b51ed75cc94fe478552
Bruno Bernardo <bruno@xhail.com>
2020-02-06 15:04:11 +0000

    add git configs

---
+++ git-config.sh
@@ -0,0 +1,10 @@
+git config --global alias.graph=log --oneline --graph
+git config --global alias.cm=commit -m
+git config --global alias.unstage=reset HEAD --
+git config --global alias.aa=add .
+git config --global color.ui=auto
+git config --global merge.tool=vimdiff
+git config --global merge.conflictstyle=diff3
+git config --global mergetool.keepbackup=false
+git config --global mergetool.prompt=false
+
