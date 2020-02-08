70e03970267ecdeedf8de0fb4461b281d52068d4
Bruno Bernardo <Bigsby@users.noreply.github.com>
2020-02-08 00:05:40 +0000

    fix syntax and add new aliases

--- git/git-config.sh
+++ git/git-config.sh
@@ -1,10 +1,14 @@
-git config --global alias.graph=log --oneline --graph
-git config --global alias.cm=commit -m
-git config --global alias.unstage=reset HEAD --
-git config --global alias.aa=add .
-git config --global color.ui=auto
-git config --global merge.tool=vimdiff
-git config --global merge.conflictstyle=diff3
-git config --global mergetool.keepbackup=false
-git config --global mergetool.prompt=false
-
+git config --global alias.graph "log --oneline --graph"
+git config --global alias.unstage "reset HEAD --"
+git config --global alias.aa "add ."
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.bra branch
+git config --global alias.ci commit
+git config --global alias.cim "commit -m"
+git config --global alias.st status
+git config --global color.ui auto
+git config --global merge.tool vimdiff
+git config --global merge.conflictstyle diff3
+git config --global mergetool.keepbackup false
+git config --global mergetool.prompt false
