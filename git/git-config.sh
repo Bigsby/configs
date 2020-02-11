9795ae6dcdaad6b2c0d13bebc35a4bed832c621a
Bruno Bernardo <bruno@xhail.com>
2020-02-11 14:51:00 +0000

    add merge --ff-only

--- git/git-config.sh
+++ git/git-config.sh
@@ -6,8 +6,9 @@ git config --global alias.br branch
 git config --global alias.bra branch
 git config --global alias.ci commit
 git config --global alias.cim "commit -m"
-git config alias.ss "status --short"
+git config --global alias.ss "status --short"
 git config --global alias.st status
+git config --global alias.mffo "merge --ff-only"
 git config --global color.ui auto
 git config --global merge.tool vimdiff
 git config --global merge.conflictstyle diff3
