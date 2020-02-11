34cb2a42d2bece906ff0a67825c4042dbfe8445e
Bruno Bernardo <bruno@xhail.com>
2020-02-11 10:24:24 +0000

    add short status alias

--- git/git-config.sh
+++ git/git-config.sh
@@ -6,6 +6,7 @@ git config --global alias.br branch
 git config --global alias.bra branch
 git config --global alias.ci commit
 git config --global alias.cim "commit -m"
+git config alias.ss "status --short"
 git config --global alias.st status
 git config --global color.ui auto
 git config --global merge.tool vimdiff
