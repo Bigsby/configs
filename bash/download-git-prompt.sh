a768ba05ce9b7c295ba5ae2dc8732ef91e60bb3d
Bruno Bernardo <bruno@xhail.com>
2020-02-11 10:47:21 +0000

    install fzf after git prompt

--- bash/download-git-prompt.sh
+++ bash/download-git-prompt.sh
@@ -1,17 +1,17 @@
 # download git promp script
 wget https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh
 
-# download fzf stuff
-git clone https://github.com/junegunn/fzf.git
-pushd fzf
-./install
-popd
-
 if [ -e ~/.bashrc ]
     then
         mv ~/.bashrc ~/.bashrc_backup
 fi
 
 cat .bashrc > ~/.bashrc
+
+# download fzf stuff
+git clone https://github.com/junegunn/fzf.git
+pushd fzf
+./install
+popd
 
 
