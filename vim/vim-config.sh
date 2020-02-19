3bf9f072105eb21699f3cd96da36c4f7e0fd8108
Bruno Bernardo <Bigsby@users.noreply.github.com>
2020-02-19 17:32:25 +0000

    remove colorscheme downloads

--- vim/vim-config.sh
+++ vim/vim-config.sh
@@ -3,10 +3,10 @@ mkdir -p ~/.vim/backup
 mkdir -p ~/.vim/swp
 mkdir -p ~/.vim/undo
 
-pushd ~/.vim/colors
-wget https://raw.githubusercontent.com/tomasiser/vim-code-dark/master/colors/codedark.vim
-wget https://github.com/sjl/badwolf/blob/master/colors/badwolf.vim
-popd
+#pushd ~/.vim/colors
+#wget https://raw.githubusercontent.com/tomasiser/vim-code-dark/master/colors/codedark.vim
+#wget https://raw.githubusercontent.com/sjl/badwolf/master/colors/badwolf.vim
+#popd
 
 # sudo apt install vim-gtk
 cp ${BASH_SOURCE%/*}/.vimrc ~/
