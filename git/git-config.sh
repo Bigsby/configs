1deb6412c1950d4c827cfac965a43c7b11a79987
Bruno Bernardo <bigsby@live.com>
2020-02-19 15:13:07 +0000

    clean up git aliases

--- git/git-config.sh
+++ git/git-config.sh
@@ -1,11 +1,11 @@
-git config --global alias.graph "log --oneline --graph"
+git config --global alias.graph "log --oneline --graph --all"
 git config --global alias.unstage "reset HEAD --"
 git config --global alias.aa "add ."
 git config --global alias.co checkout
 git config --global alias.br branch
-git config --global alias.bra branch
+git config --global alias.bra branch --all
 git config --global alias.ci commit
-git config --global alias.cim "commit -m"
+git config --global alias.cm "commit -m"
 git config --global alias.ss "status --short"
 git config --global alias.st status
 git config --global alias.mffo "merge --ff-only"
